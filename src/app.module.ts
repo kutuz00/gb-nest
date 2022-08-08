1aeb637d7e61e1646f14a842da68fc32e77ed8f5
kutuz00 <kutuz00@gmail.com>
2022-08-08 08:09:24 -0700

    News

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NewsModule } from './news/news.module';
+import { NewsModule } from './news/news.module';
+import { NewsModule } from './news/news.module';
+import { NewsModule } from './news/news.module';
 
 @Module({
-  imports: [],
+  imports: [NewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
